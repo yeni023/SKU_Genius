3097aa38bf432c0d1701952a983688f63292c1f1
yunseo <cj2174@naver.com>
2024-10-17 13:44:17 +0900

    메인화면 말풍선 수정

--- Frontend/apps/genius/src/pages/MainHome/MainHome.tsx
+++ Frontend/apps/genius/src/pages/MainHome/MainHome.tsx
@@ -94,19 +94,19 @@ const handleCreateStoryClick = () => {
       <div className="section" id="secondSection">
         <Styles.Section className="visible">
           <img src={`src/assets/images/main2.png`} alt="Main 2" />
-          <p><span className="highlight">아이</span>와 <span className="highlight2">부모</span>가 함께!<br />아이는 동화책을 만들며 성취하고,<br />부모는 아이의 동화를 자랑하세요!</p>
+          <p><span className="highlight">아이</span>와 <span className="highlight2">부모</span>가 함께하는 시간!<br />아이는 <span className="highlight3">창의적</span>인 동화책을 만들고,<br />부모는 그 성장을 응원하며 함께 즐겨보세요!</p>
         </Styles.Section>
       </div>
       <div className="section" id="thirdSection">
         <Styles.Section className="visible">
-          <p>AI를 활용해 우리 아이의 <span className="highlight"><br />창의력</span>과 <span className="highlight2">상상력</span>을 마음껏 키워봐요!</p>
+          <p>AI를 활용해 우리 아이의 <span className="highlight3"><br />창의력</span>과 <span className="highlight4">상상력</span>을 마음껏 키워봐요!</p>
           <img src={`src/assets/images/main3.png`} alt="Main 3" />
         </Styles.Section>
       </div>
       <div className="section" id="fourthSection">
         <Styles.Section className="visible">
           <img src={`src/assets/images/main4.png`} alt="Main 4" />
-          <p>아이들에게 친숙한 화면,<br />다양한 <span className="highlight">모바일 기기</span>와 <span className="highlight2">웹</span>에서도 즐길 수 있어요!</p>
+          <p>아이들에게도 친숙한 화면,<br />간편하게 동화를 제작하며 <span className="highlight5">상상의 나래</span>를 펼치세요!</p>
         </Styles.Section>
       </div>
     </Styles.AppContainer>
