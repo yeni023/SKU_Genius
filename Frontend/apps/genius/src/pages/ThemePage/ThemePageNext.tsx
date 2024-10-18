1466a5170c13e9d0c966334cdef84b78be63d82e
JY-02 <tssb7948@gmail.com>
2024-10-18 23:15:13 +0900

    themepage 코드 복원

--- Frontend/apps/genius/src/pages/ThemePage/ThemePageNext.tsx
+++ Frontend/apps/genius/src/pages/ThemePage/ThemePageNext.tsx
@@ -4,13 +4,15 @@ import { useLocation, useNavigate } from "react-router-dom";
 import ShapeNext from "../../components/ThemePage/ShapeNext";
 import * as C from "../../pages/StoryFlow/container";
 import * as Styles from "./ThemePageStyle";
-import { themes } from "./themes";
+//import { themes } from "./themes";
 
 const ThemePageNext: React.FC = () => {
   const location = useLocation();
-  const id = parseInt(new URLSearchParams(location.search).get("id") || "1");
+  const id = parseInt(new URLSearchParams(location.search).get("id") || "0");
+  const {titles} = location.state;
   const navigate = useNavigate();
   const currentPage = "ThemePageNext";
+  console.log(titles);
 
   const handleOkBtnClick = () => {
     navigate(`/SelectLevel`);
@@ -21,14 +23,14 @@ const ThemePageNext: React.FC = () => {
   };
 
   const getThemeIndex = (id: number) => {
-    return Math.ceil(id / 3) - 1;
+    return Math.floor(id / 3);
   };
 
   const themeIndex = getThemeIndex(id);
-  const startIndex = themeIndex * 3 - 1;
+  const startIndex = themeIndex * 3;
 
   const getComponentIndex = (id: number) => {
-    return (id - 1) % 3;
+    return id % 3;
   };
 
   return (
@@ -40,11 +42,11 @@ const ThemePageNext: React.FC = () => {
         <Styles.NoBtn onClick={handleNoBtnClick}>다시 고를래</Styles.NoBtn>
       </Styles.BtnContainer>
       <Styles.ShapeContainer>
-        {[1, 2, 3].map((index) => (
+        {[0, 1, 2].map((index) => (
           <ShapeNext
             key={startIndex + index}
-            title={themes[startIndex + index].title}
-            isFlower={getComponentIndex(id) === index - 1}
+            title={titles[index]}
+            isFlower={getComponentIndex(id) === index}
           />
         ))}
       </Styles.ShapeContainer>
